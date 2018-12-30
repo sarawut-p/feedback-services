d0e42d65c2a14656f111be8aae0415a8b65b18dc
Sarawut <sarawut.positwinyu@gmail.com>
2018-12-30 10:48:17 +0700

    Add cookie completed

--- server/model/User.js
+++ server/model/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  goodleId: String
+  googleId: String
 });
 
 mongoose.model('users', userSchema);
